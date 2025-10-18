cc57f346603a210beed0b3d8cf869daf5a4b4ef2
IlhamNur <romdhoninuril@gmail.com>
2025-10-18 18:56:14 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,18 +12,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'is_admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::resource('tasks', TaskController::class)->except(['create', 'show']); // kita pakai modal form via JS
-});
-
-Route::middleware(['auth', 'is_admin'])->group(function () {
-    Route::resource('users', UserController::class);
+    Route::resource('tasks', TaskController::class)->except(['create', 'show']);
 });
 
 require __DIR__ . '/auth.php';
